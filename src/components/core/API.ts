3eca11ea90802836577f4e6e93aead0e26018453
Håkon S.U <haakonu97@gmail.com>
2020-11-26 22:50:19 +0100

    Little bit of refactoring

--- src/components/core/API.ts
+++ src/components/core/API.ts
@@ -1,28 +1,24 @@
 const API_URL = `https://impedans.me`;
 
-export const GetPostBySlug = (slug: string) => {
+const doFetch = (url: string) => {
   return new Promise((resolve, reject) => {
-    fetch(`${API_URL}/web/wp-json/wp/v2/posts/?slug=${slug}`)
+    fetch(url)
       .then((res) => res.json())
       .then((res: any) => resolve(res))
       .catch((err: any) => reject(err));
   });
+};
+
+export const GetPostBySlug = (slug: string) => {
+  return doFetch(`${API_URL}/web/wp-json/wp/v2/posts/?slug=${slug}`);
 };
 
 export const GetAllPosts = (postsPerPage = 100) => {
-  return new Promise((resolve, reject) => {
-    fetch(`${API_URL}/web/wp-json/wp/v2/posts/?per_page=${postsPerPage}`)
-      .then((res) => res.json())
-      .then((res: any) => resolve(res))
-      .catch((err: any) => reject(err));
-  });
+  return doFetch(
+    `${API_URL}/web/wp-json/wp/v2/posts/?per_page=${postsPerPage}`
+  );
 };
 
 export const GetCategories = () => {
-  return new Promise((resolve, reject) => {
-    fetch(`${API_URL}/web/wp-json/wp/v2/categories`)
-      .then((res) => res.json())
-      .then((res: any) => resolve(res))
-      .catch((err: any) => reject(err));
-  });
+  return doFetch(`${API_URL}/web/wp-json/wp/v2/categories`);
 };
